4e8bf8831aca09e0f2ecfb0facc3a8532489045f
maxep <me@maxep.me>
2019-10-08 13:17:44 +0200

    Update copyright

--- MXPagerView/MXPagerViewController.m
+++ MXPagerView/MXPagerViewController.m
@@ -1,6 +1,6 @@
 // MXPagerViewController.m
 //
-// Copyright (c) 2017 Maxime Epain
+// Copyright © 2019 Maxime Epain
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
